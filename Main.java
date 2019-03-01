63361d87588b067995e95525b14812e929e3404d
Slemp123 <36300877+Slemp123@users.noreply.github.com>
2019-03-01 20:51:41 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.text.*;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
@@ -21,6 +22,8 @@ public class Main extends Application {
     public final static World world = new World(new Vec2(0.0f, -9.81f));
     private static final float WIDTH = 600;
     private static final float HEIGHT = 800;
+    public int balls = 0;
+    public int score = 0;
 
     //Convert a JBox2D x coordinate to a JavaFX pixel x coordinate
     public static float meterToPixel(float meter) {
@@ -51,11 +54,11 @@ public class Main extends Application {
         final Square squareTop = new Square(610, 1, 610, 1, 0, Color.AQUA);
         final Square squareRight = new Square(610, 810, 1, 810, 0, Color.AQUA);
         final Square squareLeft = new Square(1, 810, 1, 810, 0, Color.AQUA);
-        //final Square kickLane = new Square(587, 810, 1, 710, 0, Color.AQUA);
+        final Square kickLane = new Square(587, 810, 1, 710, 0, Color.AQUA);
         final Square topKickLane = new Square(640, 600, 200, 20, 34, Color.GREEN);
         
         
-        //final RoundThing upperRightCorner = new RoundThing(610, 20, 20, Color.RED);
+        final RoundThing upperRightCorner = new RoundThing(610, 20, 20, Color.RED);
         final RoundThing r1 = new RoundThing(175, 175, 30, Color.RED); // random ball
         final RoundThing r2 = new RoundThing(425, 175, 30, Color.RED);
         final RoundThing r3 = new RoundThing(300, 300, 30, Color.RED);
@@ -73,7 +76,7 @@ public class Main extends Application {
             }
         };
 
-
+       
 
         final Timeline timeline = new Timeline();
         timeline.setCycleCount(Timeline.INDEFINITE);
@@ -81,24 +84,29 @@ public class Main extends Application {
         Duration duration = Duration.seconds(1.0 / 60.0);
         KeyFrame frame = new KeyFrame(duration, ae, null, null);
 
+        
         timeline.getKeyFrames().add(frame);
+        Text t = new Text(10, 50, "Baller:" + balls + "   " + "Poeng:" + score);
+        t.setFont(new Font(20));
+        t.setFill(Color.WHITE);
 
         root.getChildren().add(ball.node);
         root.getChildren().add(squareBottom.node);
         root.getChildren().add(squareTop.node);
         root.getChildren().add(squareRight.node);
         root.getChildren().add(squareLeft.node);
-        //root.getChildren().add(kickLane.node);
-        //root.getChildren().add(upperRightCorner.node);
+        root.getChildren().add(kickLane.node);
+        root.getChildren().add(upperRightCorner.node);
         root.getChildren().add(r1.node);
         root.getChildren().add(r2.node);
         root.getChildren().add(r3.node);
         root.getChildren().add(topKickLane.node);
-        
+        root.getChildren().add(t);
 
-
+  
         primaryStage.setScene(scene);
         primaryStage.show();
+       
 
 
         scene.setOnKeyPressed(event -> {
